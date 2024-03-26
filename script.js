22b2ffe066a6a2b293071ad45bad208cea303311
PHO3N1XOG <160864673+PHO3N1XOG@users.noreply.github.com>
2024-03-26 19:27:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Function to display alert message
 function showAlert(message) {
     var alertContainer = document.getElementById('alert-container');
-    alertContainer.textContent = message;
+    var alertLabel = document.getElementById('alert-label');
+    alertLabel.textContent = message;
     alertContainer.style.display = 'block';
-    setTimeout(function() {
-        alertContainer.style.display = 'none';
-    }, 5000); // Hide the alert after 5 seconds
 }
 
 // Parse the command from Discord
